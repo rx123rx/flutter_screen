0924e984b19875d4fd59cd23bb4079919b94bcb4
rozar.ruan <1542666278@qq.com>
2022-01-13 00:38:07 +0800

    update the maintainer information and the channel name

--- lib/screen.dart
+++ lib/screen.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Screen {
-  static const MethodChannel _channel = const MethodChannel('github.com/clovisnicolas/flutter_screen');
+  static const MethodChannel _channel = const MethodChannel('github.com/rozar-ruan/flutter_screen');
 
   static Future<double> get brightness async => (await _channel.invokeMethod('brightness')) as double;
   static Future setBrightness(double brightness) =>_channel.invokeMethod('setBrightness',{"brightness" : brightness});
